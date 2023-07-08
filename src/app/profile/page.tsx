a82d2986a93b1101c205f39d5fe29f0c9f522f01
theshubhagrwl <shubhagrawal4457@gmail.com>
2023-07-08 12:56:32 +0530

    added profile id route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import Link from 'next/link'
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toast } from "react-hot-toast";
 
 export default function ProfilePage() {
@@ -21,15 +22,35 @@ export default function ProfilePage() {
     }
   }
 
+  const [data, setData] = useState("nothing")
+
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log(res.data);
+    setData(res.data.data._id)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile </h1>
+      <h2>
+        {data === 'nothing' ? "Nothing" :
+          <Link href={`/profile/${data}`}>{data}
+          </Link>
+        }
+
+      </h2>
       <hr />
       <button className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={logout}
       >
         Logout
       </button>
-    </div>
+      <button className="bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        onClick={getUserDetails}
+      >
+        Get User Details
+      </button>
+    </div >
   );
 }
